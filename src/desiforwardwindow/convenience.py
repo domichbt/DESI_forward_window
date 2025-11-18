09e5219de046d2a93a91ae796fe7d16dbdf55f2a
Domitille Chebat <dochebat@gmail.com>
2025-11-18 13:35:43 -0800

    add planck 2018 convenience

--- src/desiforwardwindow/convenience.py
+++ src/desiforwardwindow/convenience.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Literal
 
 import numpy as np
+from lsstypes import ObservableLeaf, ObservableTree
 
 from .utils import get_clustering_positions_weights
 
@@ -49,3 +50,39 @@ def get_randoms(
     if weight_type is None:
         weights = [np.ones_like(weights[0])]
     return positions, weights
+
+
+def fiducial_planck_2018(edgesin: np.ndarray) -> ObservableTree:
+    """
+    Get the power spectrum of Planck 2018 flat LCDM cosmology, with required k and Kaiser RSD.
+
+    Parameters
+    ----------
+    edgesin : np.ndarray
+        Edges of the bins for the power spectrum.
+
+    Returns
+    -------
+    ObservableTree
+        Matter power spectrum with Kaiser RSD based on the Planck 2018 flat LCDM cosmology.
+    """
+    from cosmoprimo.fiducial import Planck2018FullFlatLCDM
+
+    # Use a basic linear P(k) with RSD multipoles for the fiducial cosmology
+    cosmo = Planck2018FullFlatLCDM(engine="eisenstein_hu")
+    pk = cosmo.get_fourier().pk_interpolator().to_1d(z=1.0)
+
+    edgesin = np.column_stack([edgesin[:-1], edgesin[1:]])
+
+    kin = np.mean(edgesin, axis=-1)
+    pkin = pk(kin)
+    ells = (0, 2, 4)
+
+    f, b = 0.8, 1.5
+    pkb = b**2 * pkin
+    beta = f / b
+    poles = [(1.0 + 2.0 / 3.0 * beta + 1.0 / 5.0 * beta**2) * pkb, 0.9 * (4.0 / 3.0 * beta + 4.0 / 7.0 * beta**2) * pkb, 8.0 / 35 * beta**2 * pkb]
+    poles = np.array(poles)
+
+    theory = ObservableTree([ObservableLeaf(k=kin, k_edges=edgesin, value=pole, coords=["k"]) for pole in poles], ells=ells)
+    return theory
