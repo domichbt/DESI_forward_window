eea54d4290bd52036b8adc81d6a9306405ee0a50
Domitille Chebat <domitille.chebat@gmail.com>
2026-02-20 15:21:08 +0100

    update deps, handle missing convenience deps in __init__

--- src/desiwinds/__init__.py
+++ src/desiwinds/__init__.py
@@ -1,1 +1,8 @@
-from . import convenience, forward, window
+from . import forward, window
+
+try:
+    from . import convenience
+except ImportError as e:
+    import warnings
+
+    warnings.warn(f"Could not import convenience function, likely due to missing dependencies: {e}", stacklevel=2)
